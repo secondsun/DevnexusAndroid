6b97aa7175750a3d158c1a8261c2a89ed5e7d2a4
Summers Pittman <secondsun@gmail.com>
2013-12-17 20:15:25 -0500

    Refactoring URLs

--- DevNexus/src/main/java/org/devnexus/DevnexusApplication.java
+++ DevNexus/src/main/java/org/devnexus/DevnexusApplication.java
@@ -1,5 +1,6 @@
 package org.devnexus;
 
+import android.app.Activity;
 import android.app.Application;
 import android.os.Handler;
 import android.util.Log;
@@ -41,6 +42,7 @@ import org.jboss.aerogear.android.sync.*;
 import java.lang.reflect.Type;
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -58,11 +60,12 @@ public class DevnexusApplication extends Application {
     private static final URL DEVNEXUS_URL;
 
     private static final String TAG = DevnexusApplication.class.getSimpleName();
+    private static final URI PUSH_URL;
     public static DevnexusApplication CONTEXT = null;
     private SQLStore<Schedule> scheduleStore;
     private SQLStore<UserCalendar> userCalendarStore;
     private SQLStore<SyncStats> statsStore;
-    private Authenticator authenticator = new Authenticator("http://192.168.1.194:9090");
+    private Authenticator authenticator;
     private TwoWaySqlSynchronizer<UserCalendar> userCalendarSync;
 
     private GsonBuilder builder = new GsonBuilder();
@@ -82,15 +85,21 @@ public class DevnexusApplication extends Application {
 
     static {
         try {
-            DEVNEXUS_URL = new URL("http://192.168.1.194:9090/s/devnexus2013/");
+            DEVNEXUS_URL = new URL("http://10.0.2.2:8080/s/");
+            PUSH_URL = new URI("http://192.168.1.194:8080/ag-push");
         } catch (MalformedURLException e) {
             Log.e(TAG, e.getMessage(), e);
             throw new RuntimeException(e);
+        } catch (URISyntaxException e) {
+            Log.e(TAG, e.getMessage(), e);
+            throw new RuntimeException(e);
         }
+
     }
 
     {
         pipeline = new Pipeline(DEVNEXUS_URL);
+        authenticator = new Authenticator(DEVNEXUS_URL);
 
         builder.registerTypeAdapter(Date.class, new JsonDeserializer() {
             public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
@@ -117,14 +126,9 @@ public class DevnexusApplication extends Application {
 
         AuthenticationConfig config = new AuthenticationConfig();
 
-        config.setLoginEndpoint("s/loginAndroid.json");
+        config.setLoginEndpoint("loginAndroid.json");
 
-        GooglePlusAuthenticationModule module = null;
-        try {
-            module = new GooglePlusAuthenticationModule(new URL("http://192.168.1.194:9090/"), config, getApplicationContext());
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
+        GooglePlusAuthenticationModule module = new GooglePlusAuthenticationModule(DEVNEXUS_URL, config, getApplicationContext());
 
         authenticator.add("google", module);
 
@@ -175,7 +179,7 @@ public class DevnexusApplication extends Application {
 
 
         // Create a PushConfig for the UnifiedPush Server:
-        pushConfig = new PushConfig(URI.create("http://192.168.1.194:8080/ag-push"), "402595014005");
+        pushConfig = new PushConfig(PUSH_URL, "402595014005");
         pushConfig.setVariantID("a26c1609-873e-427e-9106-7a6d435cbc78");
         pushConfig.setSecret("5264fdc8-f0e6-480a-8725-f24caa9440e5");
 
@@ -310,7 +314,7 @@ public class DevnexusApplication extends Application {
     }
 
 
-    public AuthenticationModule getAuth(MainActivity mainActivity) {
+    public AuthenticationModule getAuth(Activity mainActivity) {
         return authenticator.get("google", mainActivity);
     }
 
